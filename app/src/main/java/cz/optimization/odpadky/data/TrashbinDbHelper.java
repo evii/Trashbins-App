510d7e380f1dbea2b064c0c2066bc9812dff96bb
evii <evi.pavelkova@gmail.com>
2018-07-21 21:23:45 +0200

    Stage before Capstone

--- app/src/main/java/cz/optimization/odpadky/data/TrashbinDbHelper.java
+++ app/src/main/java/cz/optimization/odpadky/data/TrashbinDbHelper.java
@@ -183,7 +183,7 @@ public class TrashbinDbHelper extends SQLiteOpenHelper {
 
             if (cursor != null) {
 
-                itemsList = addPoints(cursor);
+                itemsList = ParseTrashbinLocation.getTrashbinLocation(cursor);
             }
 
             return itemsList;
@@ -196,70 +196,7 @@ public class TrashbinDbHelper extends SQLiteOpenHelper {
         }
     }
 
-    private List<TrashbinClusterItem> addPoints(Cursor cursor) {
 
-        List<TrashbinClusterItem> ListItems = new ArrayList<>();
-
-        int locationCount = 0;
-        String address = "";
-        double lat = 0;
-        double lng = 0;
-        double progress = 0;
-        //   String trashType = "";
-        //    Float colour = BitmapDescriptorFactory.HUE_ROSE;
-        String snippet = "";
-
-        // Number of locations available in the SQLite database table
-        locationCount = cursor.getCount();
-
-        // Move the current record pointer to the first row of the table
-        cursor.moveToFirst();
-
-        for (int i = 0; i < locationCount; i++) {
-
-            lat = cursor.getDouble(cursor.getColumnIndex(TrashbinContract.TrashbinEntry.COLUMN_LAT));
-            lng = cursor.getDouble(cursor.getColumnIndex(TrashbinContract.TrashbinEntry.COLUMN_LONG));
-
-            address = cursor.getString(cursor.getColumnIndex(TrashbinContract.TrashbinEntry.COLUMN_ADDRESS));
-            progress = cursor.getDouble(cursor.getColumnIndex(TrashbinContract.TrashbinEntry.COLUMN_PROGRESS));
-            double progressRounded = (double) Math.round(progress * 100) / 100;
-            snippet = "Naplněnost: " + (progressRounded * 100) + " %";
-
-            // Getting colour of the point
-            /** trashType = cursor.getString(cursor.getColumnIndex(TrashbinContract.TrashbinEntry.COLUMN_TRASHTYPE_INDEX));
-
-             switch (trashType) {
-             case "BS":
-             colour = BitmapDescriptorFactory.HUE_GREEN;
-             break;
-             case "CS":
-             colour = BitmapDescriptorFactory.HUE_AZURE;
-             break;
-             case "K":
-             colour = BitmapDescriptorFactory.HUE_MAGENTA;
-             break;
-             case "NK":
-             colour = BitmapDescriptorFactory.HUE_ORANGE;
-             break;
-             case "PA":
-             colour = BitmapDescriptorFactory.HUE_BLUE;
-             break;
-             case "PL":
-             colour = BitmapDescriptorFactory.HUE_YELLOW;
-             break;
-             }
-             */
-
-            // Adding the marker to the List
-            ListItems.add(new TrashbinClusterItem(lat, lng, address, snippet));
-
-            // Traverse the pointer to the next row
-            cursor.moveToNext();
-        }
-
-        cursor.close();
-        return ListItems;
-    }
 
     public class FetchSelectedLocations extends AsyncTask<String[], Void, List<TrashbinClusterItem>> {
 
@@ -274,7 +211,7 @@ public class TrashbinDbHelper extends SQLiteOpenHelper {
             Cursor cursor = myContext.getContentResolver().query(TrashbinContract.TrashbinEntry.CONTENT_URI, null, selection, selArgs,
                     null);
 
-            itemsList = addPoints(cursor);
+            itemsList = ParseTrashbinLocation.getTrashbinLocation(cursor);
             return itemsList;
         }
 
